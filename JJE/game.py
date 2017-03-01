4b085fb3aab3b24be31ccf0765805f7677f49f44
jdub01010101 <jdub01010101@gmail.com>
2017-02-28 20:37:32 -0600

    started adding directions and inventory

--- JJE/game.py
+++ JJE/game.py
@@ -1,17 +1,23 @@
 # This is the new main game file.
 def play():
+    inventory = ['Blunt Sword', 'Pennies(2)', 'Moldy Bread']
     print("Escape from the abandoned mansion!")
-    action_input = get_player_command()
-    if action_input == 'n' or action_input == 'N':
-        print("Go North!")
-    elif action_input == 's' or action_input == 'S':
-        print("Go South!")
-    elif action_input == 'e' or action_input == 'E':
-        print("Go East!")
-    elif action_input == 'w' or action_input == 'W':
-        print("Go West young man!")
-    else:
-        print("Invalid option!")
+    while True:
+        action_input = get_player_command()
+        if action_input in ['n', 'N']:
+            print("Go North!")
+        elif action_input in ['s', 'S']:
+            print("Go South!")
+        elif action_input in ['e', 'E']:
+            print("Go East!")
+        elif action_input in ['w', 'W']:
+            print("Go West young man!")
+        elif action_input in ['i', 'I']:
+            print("Inventory: ")
+            for item in inventory:
+                print('* ' + str(item))
+        else:
+            print("Invalid option!")
 
 def get_player_command():
     return input('Action: ')
