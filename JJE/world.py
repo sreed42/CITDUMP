9bb0a22e471b25f6613ebb65c7ec6ed4ed8bfcbb
jdub01010101 <jdub01010101@gmail.com>
2017-02-28 21:59:04 -0600

    World setup

---
+++ JJE/world.py
@@ -0,0 +1,45 @@
+#Welcome to the world as defined by JJE.
+
+class MapTile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def intro_text(self):
+        raise NotImplementedError("Create a subclass!!")
+
+class StartTile:
+    def intro_text(self):
+        return """
+        You are locked in an abandoned mansion.
+        There are a few paths to travel.
+        Can you survive?
+        """
+
+class BoringTile(MapTile):
+    def intro_text(self):
+        return """
+        Not much is happening here.
+        """
+
+class VictoryTile(MapTile):
+    def intro_text(self):
+        return """
+        Escape is in your grasp!
+        You survived!!!!
+        """
+
+world_map = [
+    [None,VictoryTile(1,0),None],
+    [None,BoringTile(1,1),None],
+    [BoringTile(0,2),StartTile(1,2),BoringTile(2,2)]
+    [None,BoringTile(1,3),None]
+]
+
+def tile_at(x,y):
+    if x < 0 or y < 0:
+        return None
+    try:
+        return world_map[y][x]
+    except IndexError:
+        return None
