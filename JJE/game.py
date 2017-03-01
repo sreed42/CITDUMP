9e41785744d856c0de9130dc70a84a692c294959
jdub01010101 <jdub01010101@gmail.com>
2017-02-28 21:18:17 -0600

    added additional functionality

--- JJE/game.py
+++ JJE/game.py
@@ -1,33 +1,29 @@
 # This is the new main game file.
-class Rock:
+class Weapon:
+    def __str__(self):
+        return self.name
+
+class Rock(Weapon):
     def __init__(self):
         self.name = "Rock"
         self.description = "I guess this is a weapon. " \
                 "About the size of your hand."
         self.damage = 1
-    def __str__(self):
-        return self.name
 
-class Blunt_Sword:
+class Blunt_Sword(Weapon):
     def __init__(self):
         self.name = "Blunt Sword"
         self.description = "More useful as a club than a cutting tool."
         self.damage = 5
 
-    def __str__(self):
-        return self.name
-
-class Pistol:
+class Pistol(Weapon):
     def __init__(self):
         self.name = "9mm Pistol"
         self.description = "Bang bang!"
         self.damage = 20
 
-    def __str__(self):
-        return self.name
-
 def play():
-    inventory = [Blunt_Sword(), 'Pennies(2)', 'Moldy Bread']
+    inventory = [Rock(), Blunt_Sword(), 'Pennies(2)', 'Moldy Bread']
     print("Escape from the abandoned mansion!")
     while True:
         action_input = get_player_command()
@@ -48,5 +44,17 @@ def play():
 
 def get_player_command():
     return input('Action (N, S, E, W, I: ')
+
+def strongest_weapon(inventory):
+    max_damage = 0
+    best_weapon = None
+    for item in inventory:
+        try:
+            if item.damage > max_damage:
+                best_weapon = item
+                max_damage = item.damage
+        except AttributeError:
+            pass
+    return best_weapon
 
 play()
