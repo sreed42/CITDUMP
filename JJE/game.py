39c7a4f132150a07e2b2d91a40372e5e92ddbeb4
jdub01010101 <jdub01010101@gmail.com>
2017-02-28 22:08:43 -0600

    Added notification of room.

--- JJE/game.py
+++ JJE/game.py
@@ -1,20 +1,24 @@
 # This is the new main game file.
 
 from player import Player
+import world
+
 
 def play():
     print("Escape from the abandoned mansion!")
     player = Player()
     while True:
+        room = world.tile_at(player.x, player.y)
+        print(room.intro_text())
         action_input = get_player_command()
         if action_input in ['n', 'N']:
-            print("Go North!")
+            player.move_north()
         elif action_input in ['s', 'S']:
-            print("Go South!")
+            player.move_south()
         elif action_input in ['e', 'E']:
-            print("Go East!")
+            player.move_east()
         elif action_input in ['w', 'W']:
-            print("Go West young man!")
+            player.move_west()
         elif action_input in ['i', 'I']:
             player.print_inventory()
         else:
