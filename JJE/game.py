99c864c4d4c5ac6bfb42f12af3046aceda1678bd
eruiz89 <lizthornton07@hotmail.com>
2017-03-09 16:36:04 -0600

    Update game.py

--- JJE/game.py
+++ JJE/game.py
@@ -1,8 +1,10 @@
-#Game file for JJE
+# Game file for JJE
+# This is how the game functions
 
 from player import Player
-import world
 from collections import OrderedDict
+import world
+
 
 def play():
     print("Escape from the abandoned mansion!")
@@ -15,31 +17,18 @@ def play():
         if player.is_alive() and not player.victory:
             choose_action(room, player)
         elif not player.is_alive():
-            print("Your journey has come to an early end!")
-    while True:
-        room = world.tile_at(player.x, player.y)
-        print(room.intro_text())
-        room.modify_player(player)
-        action_input = get_player_command()
-        if action_input in ['n', 'N']:
-            player.move_north()
-        elif action_input in ['s', 'S']:
-            player.move_south()
-        elif action_input in ['e', 'E']:
-            player.move_east()
-        elif action_input in ['w', 'W']:
-            player.move_west()
-        elif action_input in ['i', 'I']:
-            player.print_inventory()
-        elif action_input in ['a', 'A']:
-            player.attack()
-        elif action_input in ['h', 'H']:
-            player.heal()
-        else:
-            print("Invalid option!")
+            print("Your journey has been cut short!")
 
-def get_player_command():
-    return input('Action (N, S, E, W, I: ')
+def choose_action(room, player):
+    action = None
+    while not action:
+        available_actions = get_available_actions(room, player)
+        action_input = input("Action: ")
+        action = available_actions.get(action_input)
+        if action:
+            action()
+        else:
+            print("Invalid action!")
 
 def get_available_actions(room, player):
     actions = OrderedDict()
@@ -51,16 +40,16 @@ def get_available_actions(room, player):
     if isinstance(room, world.EnemyTile) and room.enemy.is_alive():
         action_adder(actions, 'a', player.attack, "Attack")
     else:
-        if world.title_at(room.x, room.y - 1):
-            action_adder(actions, 'n', player.move_north, "Go north")
-        if world.title_at(room.x, room.y + 1):
-            action_adder(actions, 's', player.move_north, "Go south")
-        if world.title_at(room.x + 1, room.y):
-            action_adder(actions, 'e', player.move_north, "Go east")
-        if world.title_at(room.x - 1, room.y):
-            action_adder(actions, 'w', player.move_north, "Go west")
-    if player.hp < 100:
-        action_adder(actions, 'h', player.heal, "Heal")
+        if world.tile_at(room.x, room.y - 1):
+            action_adder(actions, 'n', player.move_north, "Go North")
+        if world.tile_at(room.x, room.y + 1):
+            action_adder(actions, 's', player.move_south, "Go South")
+        if world.tile_at(room.x + 1, room.y):
+            action_adder(actions, 'e', player.move_east, "Go East")
+        if world.tile_at(room.x - 1, room.y):
+            action_adder(actions, 'w', player.move_west, "Go West")
+        if player.hp < 100:
+            action_adder(actions, 'h', player.heal, "Heal")
     return actions
 
 def action_adder(action_dict, hotkey, action, name):
@@ -68,16 +57,13 @@ def action_adder(action_dict, hotkey, action, name):
     action_dict[hotkey.upper()] = action
     print("{}: {}".format(hotkey, name))
 
-def choose_action(room, player):
-    action = None
-    while not action:
-        available_action = get_available_actions(room, player)
-        action_input = input("Action: ")
-        action = available_action.get(action_input)
-        if action:
-            action()
-        else:
-            print("Invalid action!")
+def get_player_command():
+    return input('Action: (N, S, E, W, I, A, H: ')
 
 play()
 
+# while True:
+    # room = world.tile_at(player.x(), player.y)
+    # print(room.intro_text())
+    # room.modify_player(Player)
+    # action_input = get_player_command()
