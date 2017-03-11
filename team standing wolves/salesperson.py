3fe426bbbd2c9d1e6b4b9a777ae8c1f0f4dc99d0
richiemar <ramarier@schreiner.edu>
2017-03-11 00:19:40 -0600

    Add files via upload

---
+++ team standing wolves/salesperson.py
@@ -0,0 +1,19 @@
+class salesperson(object):  # creating salesperson class
+
+    def __init__(self, name, idnum, gender, age):
+        self.name = name
+        self.idnum = idnum
+        self.gender = gender
+        self.age = age
+
+    def getname(self):
+        return self.name
+
+    def getID(self):
+        return self.idnum
+
+    def getGender(self):
+        return self.gender
+
+    def getAge(self):
+        return self.age
