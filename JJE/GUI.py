f2b8e9ad520674e9c8e7b4e47ad3f0a4774c93d0
jdub01010101 <jdub01010101@gmail.com>
2017-04-11 18:36:07 -0500

    GUI stuff

--- JJE/GUI.py
+++ JJE/GUI.py
@@ -1,15 +1,30 @@
 import main
-import gi
 
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from Tkinter import *
 
 
-builder = Gtk.Builder()
-builder.add_from_file("MansionGUI.glade")
-#builder.connect_signals(Handler())
+root = Tk()
+root.title("Escape from Mansion!")
 
-window = builder.get_object("applicationwindow1")
-window.show_all()
+root.mainloop()
 
-Gtk.main()
+NorthButton = Button(root, text="North")
+SouthButton = Button(root, text="South")
+EastButton =  Button(root, text="East")
+WestButton =  Button(root, text="West")
+
+HealButton =  Button(root, text="Heal")
+InventoryButton = Button(root, text="Inventory")
+AttackButton = Button(root, text="Attack")
+
+Text = Entry(root)
+listbox = Listbox(root)
+
+Text.pack()
+NorthButton.pack()
+SouthButton.pack()
+EastButton.pack()
+WestButton.pack()
+HealButton.pack()
+InventoryButton.pack()
+AttackButton.pack()
