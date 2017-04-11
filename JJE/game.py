93e8bb113bb120b2a2c342233cf0a75903b8f0c8
jdub01010101 <jdub01010101@gmail.com>
2017-04-11 16:20:21 -0500

    Load gui on screen

--- JJE/game.py
+++ JJE/game.py
@@ -5,10 +5,19 @@ from player import Player
 from collections import OrderedDict
 import world
 import gi
+
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 
+builder = Gtk.Builder()
+builder.add_from_file("MansionGUI.glade")
+#builder.connect_signals(Handler())
+
+window = builder.get_object("applicationwindow1")
+window.show_all()
+
+Gtk.main()
 
 def play():
     print("Escape from the abandoned mansion!")
